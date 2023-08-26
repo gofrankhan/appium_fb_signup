7ae7b8d4f992044153a6441ec9524fd63d4746dc
Md. Gofran Khan <gofran0123@gmail.com>
2023-08-27 00:17:33 +0600

    all steps to complete signup process automation

--- src/test/java/appiumtests/FacebookTest.java
+++ src/test/java/appiumtests/FacebookTest.java
@@ -3,15 +3,19 @@ package appiumtests;
 import java.net.URL;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidKeyCode;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.remote.MobilePlatform;
+import io.appium.java_client.touch.offset.PointOption;
 
 public class FacebookTest {
 	
@@ -71,12 +75,26 @@ public class FacebookTest {
 //		driver.findElement(By.xpath("//android.widget.NumberPicker[@index='2']")).sendKeys("May"); 
 		
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("android:id/button1")));
-		driver.findElement(By.xpath("//android.widget.NumberPicker[@index='2']")).sendKeys("2000");
+		
+		MobileElement startElement = driver.findElement(By.xpath("//android.widget.NumberPicker[2]/android.widget.EditText"));
+
+		// Define the coordinates for the swipe start and end points
+		int startX = startElement.getLocation().getX() + startElement.getSize().getWidth() / 2;
+		int startY = startElement.getLocation().getY() + startElement.getSize().getHeight() / 2;
+		int endX = startX; // Same X coordinate as start
+		int endY = startY - 30; // Adjust this value to control the swipe distance
+
+		// Perform the swipe gesture
+		TouchAction touchAction = new TouchAction(driver);
+		touchAction.press(PointOption.point(startX, startY))
+		           .moveTo(PointOption.point(endX, endY))
+		           .release()
+		           .perform();
 		
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("android:id/button1")));
 		MobileElement btnSet = driver.findElement(By.id("android:id/button1")); 
 		btnSet.click();
-		
+	
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.view.View[@content-desc=\"Next\"]")));
 		MobileElement btnNext1 = driver.findElement(By.xpath("//android.view.View[@content-desc=\"Next\"]"));
 		btnNext1.click();
@@ -85,15 +103,29 @@ public class FacebookTest {
 		MobileElement btnMale = driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Male\"]"));
 		btnMale.click();
 		
-		btnNext1.click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.view.View[@content-desc=\"Next\"]")));
+		MobileElement btnNext2 = driver.findElement(By.xpath("//android.view.View[@content-desc=\"Next\"]"));
+		btnNext2.click();
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.Button[1]")));
+		MobileElement btnDeny = driver.findElement(By.xpath("//android.widget.Button[1]"));
+		btnDeny.click();
 		
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.EditText")));
 		MobileElement inputMobile = driver.findElement(By.xpath("//android.widget.EditText"));
-		inputMobile.sendKeys("017002233344");
+		inputMobile.sendKeys("01700224466");
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.view.View[@content-desc=\"Next\"]")));
+		MobileElement btnNext3 = driver.findElement(By.xpath("//android.view.View[@content-desc=\"Next\"]"));
+		btnNext3.click();
 		
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.EditText")));
-		MobileElement inputPassword = driver.findElement(By.xpath("/android.widget.EditText"));
+		MobileElement inputPassword = driver.findElement(By.xpath("//android.widget.EditText"));
 		inputPassword.sendKeys("12qw!@QW");
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.view.View[@content-desc=\"Next\"]")));
+		MobileElement btnNext4 = driver.findElement(By.xpath("//android.view.View[@content-desc=\"Next\"]"));
+		btnNext4.click();
 		
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.Button[@content-desc=\"Save\"]")));
 		MobileElement btnSave = driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Save\"]"));
